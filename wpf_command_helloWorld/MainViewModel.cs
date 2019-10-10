89df81d249c9af3a5fe848fa9000a278dd5f1ae0
nbourre <nbourre@hotmail.com>
2019-10-10 14:34:33 -0400

    Ajout du projet HelloWorld

---
+++ wpf_command_helloWorld/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpf_command_helloWorld
+{
+    class MainViewModel : BaseViewModel
+    {
+        private string _message;
+
+        public string Message {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowMessageCommand { get; set; }
+
+        public MainViewModel()
+        {
+            ShowMessageCommand = new DelegateCommand<string>(ShowMessage, CanShowMessage);
+        }
+
+        private bool CanShowMessage(string obj)
+        {
+            return true;
+        }
+
+        private void ShowMessage(string obj)
+        {
+            Message = obj;
+        }
+    }
+}
--- wpf_demo_commands/ViewModels/PersonViewModel.cs
+++ wpf_demo_commands/ViewModels/PersonViewModel.cs
@@ -8,11 +8,9 @@ namespace wpf_demo_commands.ViewModels
 {
     class PersonViewModel
     {
-        private ObservableCollection<Person> _personList;
-
         public PersonViewModel()
         {
-            _personList = new ObservableCollection<Person>()
+            Persons = new ObservableCollection<Person>()
         {
             new Person(){Name="Nicolas", Address="Shawinigan"},
             new Person(){Name="Sylvain",Address="Trois-Rivières"}
@@ -20,11 +18,7 @@ namespace wpf_demo_commands.ViewModels
 
         }
 
-        public ObservableCollection<Person> Persons
-        {
-            get { return _personList; }
-            set { _personList = value; }
-        }
+        public ObservableCollection<Person> Persons { get; set; }
 
         private ICommand mUpdater;
         
